45e701e1a9cecf9cd987f68953d509c3da999fca
NINEM4RE <77124386+NINEM4RE@users.noreply.github.com>
2025-08-09 10:24:54 +0200

    Delete scripts directory

--- scripts/miscellaneous.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener('load', function() {
-    const profileContainer = document.getElementById('profile-container');
-    profileContainer.style.transform = 'translateZ(0)';
-    profileContainer.style.opacity = '99.9%'; // Force a tiny change
-    setTimeout(() => {
-        profileContainer.style.transform = '';
-        profileContainer.style.opacity = '100%';
-    }, 10);
-});
--- scripts/tilt-elements.js
+++
@@ -1,9 +0,0 @@
-VanillaTilt.init(document.querySelectorAll(".socials img"), {
-    max: 20,
-    speed: 1000,
-    reverse: false,
-    perspective: 200,
-    glare: true,
-    scale: 1.2,
-    transition: false
-});
